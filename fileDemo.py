c8f56a4fa687e7e832bf32e9135bb695b6a6f747
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-06 13:48:34 -0500

    Update fileDemo.py

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#Erik Hansen
+#12/6/17
+#fileDemo.py - how to read a file
 
+dictionary = open('engmix.txt')
+
+wordCount = 0
+for word in dictionary:
+    if 'hans' in word:
+        print(word.strip())
+    wordCount += 1
+    
+print('There are', wordCount,'words in the dictionary.')
