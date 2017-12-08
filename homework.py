124f274b6d366054555dceda16f2bbc9fe8c7a28
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-08 14:38:55 -0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,1 +1,16 @@
+#Erik Hansen
+#12/8/17
+#homework.py
 
+dictionary = open('engmix.txt')
+tf = False
+word = input('Enter a word: ')
+for words in dictionary:
+    if word == words.strip():
+        tf = True
+
+
+if tf == True:
+    print(word, 'is in the dictionary')
+else:
+    print(word,'is not in the dictionary')
