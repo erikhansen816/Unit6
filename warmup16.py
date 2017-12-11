36181b0e1c50750585276b39e931a9f9699ce733
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-11 14:28:11 -0500

    Update warmup16.py

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,14 @@
+#Erik Hansen
+#12/11/17
+#warmup16.py
 
+firstletter = input('What is the first letter of your name: ')
+lastletter = input('What is the first letter of your last name: ')
+
+dictionary = open('engmix.txt')
+
+for i in dictionary:
+    if i != '':
+        if i.strip()[0] == firstletter and i.strip()[-1] == lastletter:
+            print(i.strip())
+    
