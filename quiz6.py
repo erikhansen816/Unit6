473e52ed0242236838552ab5c152952c2510b205
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-13 13:30:05 -0500

    Updated from Brython Server: 12/13/2017 1:30:04 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,10 @@
+#Erik Hansen
+#12/13/17
+#quiz6.py
 
+"""Program 1
+dictionary = open('engmix.txt')
+for word in dictionary:
+    if word.count('c') == 3 and word.count('p') == 2:
+        print(word.strip())
+"""        
