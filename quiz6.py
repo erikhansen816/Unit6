dd5d8be07944cabd28c7165dcb2763547dac7719
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-13 13:39:35 -0500

    Updated from Brython Server: 12/13/2017 1:39:34 PM

--- quiz6.py
+++ quiz6.py
@@ -21,12 +21,22 @@ for word in dictionary:
 print(total)
 """
 
-#Program 3
+"""Program 3
 dictionary = open('engmix.txt')
-number = int(input('Enter a number between 1 and 22: '))
+number = int(input('Enter a number between 1 and 21: '))
 for word in dictionary:
     if word != '':
         if len(word) == number+1:
             print(word)
             break
-            
+"""
+
+#Program 4
+dictionary = open('engmix.txt')
+letter = input('Enter a letter: ')
+total = 0
+for word in dictionary:
+    if word != '':
+        if letter not in word:
+            total += 1
+print(total)
