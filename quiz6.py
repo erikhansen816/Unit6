dec38aa7415de5910558200c17d15df62eebc22a
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-12-13 13:34:04 -0500

    Updated from Brython Server: 12/13/2017 1:34:03 PM

--- quiz6.py
+++ quiz6.py
@@ -4,7 +4,31 @@
 
 """Program 1
 dictionary = open('engmix.txt')
+
 for word in dictionary:
-    if word.count('c') == 3 and word.count('p') == 2:
-        print(word.strip())
+    if word != '':
+        if word.count('c') == 3 and word.count('p') == 2:
+            print(word.strip())
 """        
+
+"""Program 2
+dictionary = open('engmix.txt')
+total = 0
+for word in dictionary:
+    if word != '':
+        if word.strip()[0] == 'r':
+            total += 1
+print(total)
+"""
+
+Program 3
+dictionary = open('engmix.txt')
+number = input('Enter a number between 1 and 22: ')
+total = 0
+while total <= 1
+    for word in dictionary:
+        if word != '':
+            if len(word) == number:
+                total += 1
+                print(word)
+            
